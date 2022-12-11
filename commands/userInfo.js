4de16d05db66a4c119e4c214fed106c950b8b146
AonghasMackay <aonghasmackay@yahoo.co.uk>
2022-12-11 16:37:16 +0000

    comment userInfo

--- commands/userInfo.js
+++ commands/userInfo.js
@@ -8,24 +8,33 @@ module.exports = {
 		.setName('userinfo')
 		.setDescription('Displays information about you. e.g. your balance'),
 	async execute(interaction) {
-		const user = await users.prototype.getUser(interaction.user.id);
-
-		//cast user properties to string
-		const userBalance = String(user.balance);
-		const userScore = String(user.score);
-		const userNumberOfWins = String(user.number_of_wins);
-		const userSilencedBot = String(user.silenced_bot);
-
-		const userInfoEmbed = new EmbedBuilder()
-			.setColor(0x7842f5)
-			.setTitle('User Info - ' + interaction.user.username)
-			.addFields(
-				{ name: 'Balance', value: userBalance },
-				{ name: 'Current Score', value: userScore },
-				{ name: 'Number of Wins', value: userNumberOfWins },
-				{ name: 'Bot muted?', value: userSilencedBot },
-			);
-
-		await interaction.reply({ embeds: [userInfoEmbed] });
+		sendUserInfoEmbed(interaction);
 	},
 };
+
+/**
+ * Sends an embed with the users information
+ *
+ * @param {DiscordAPIError.interaction} interaction
+ */
+async function sendUserInfoEmbed(interaction) {
+	const user = await users.prototype.getUser(interaction.user.id);
+
+	//cast user properties to string
+	const userBalance = String(user.balance);
+	const userScore = String(user.score);
+	const userNumberOfWins = String(user.number_of_wins);
+	const userSilencedBot = String(user.silenced_bot);
+
+	const userInfoEmbed = new EmbedBuilder()
+		.setColor(0x7842f5)
+		.setTitle('User Info - ' + interaction.user.username)
+		.addFields(
+			{ name: 'Balance', value: userBalance },
+			{ name: 'Current Score', value: userScore },
+			{ name: 'Number of Wins', value: userNumberOfWins },
+			{ name: 'Bot muted?', value: userSilencedBot },
+		);
+
+	await interaction.reply({ embeds: [userInfoEmbed] });
+}
