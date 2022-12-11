5f19cc003b154294653c2ada99d845a94b0d7a0f
AonghasMackay <aonghasmackay@yahoo.co.uk>
2022-12-11 18:01:29 +0000

    formatting change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //Imports
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 const { createResetBalanceCronJob } = require('./cronJobs/resetBalance.js');
